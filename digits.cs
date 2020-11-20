f6cd899a2f1915a35dfbafeaa5fc30dc24e1ba5d
javisk8203 <javialfaroh@gmail.com>
2020-11-20 19:31:13 +0100

    Fixed variable names and horizontal/vertical spacing

--- digits.cs
+++ digits.cs
@@ -2,34 +2,36 @@ using System;
 
 class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
+    static void Main()
+    {
+        //counters of number of digits "one" is for one digit, "two" is for 2 digits, "three" is for 3 digits and "more" is for more than 3 digits
+        int one = 0, two = 0, three = 0, more = 0;
+
+        Console.Write("Insert a number: ");
+        
+        // read the number as string to check the word "end"
+        string end = Console.ReadLine();
+
+        while(end !="end")
+        {
+            int input = Convert.ToInt32(end);
+
+            if(input != 0)
+            {
+                if(input/10 == 0)
+                    one ++;  // it only has one digit
+                else 
+                if (input/100 == 0)
+                    two ++;  // the number has 2 digits
+                    else
+                    if (input/1000 == 0)
+                    three ++;  // the number has 3 digits
+                    else
+                    more++;  // the number has more than 3 digits
+            }
+            end = Console.ReadLine();
         }
-      e = Console.ReadLine();
-}
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
+        Console.WriteLine("one digit: {0} two digits: {1} three digits: {2} more digits: {3}", one, two, three, more); 
+    }
 }
 
