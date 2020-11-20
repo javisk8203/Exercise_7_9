b42c3eecdfa39aa6b261e70777577efea65b78fe
javisk8203 <javialfaroh@gmail.com>
2020-11-20 19:10:50 +0100

    Fixed variable names and horizontal/vertical spacing

--- average.cs
+++ average.cs
@@ -1,20 +1,26 @@
 using System;
+
 public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+    public static void Main(){
+
+        double sum = 0, note, count = 0, average;
+        string mark;
+
+        // we read the mark as string to check the word "end"
+        Console.Write("Insert a mark: ");
+        mark = Console.ReadLine();
+
+        // if the mark isn't an end then we pass it to double to work with it
+        while(mark != "end"){  
+            note = Convert.ToDouble(mark);
+            count++;   //counter of numbers
+            sum += note;  // sum of all the notes
+            Console.Write("Insert a mark: ");
+            mark = Console.ReadLine();
+        }
+        if(count!= 0){
+            average = sum / count;  //calculate the average
+            Console.WriteLine("The average is {0}.",average);
+        }
+    }
 }
